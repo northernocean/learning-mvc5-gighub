5717b70d657490ce7ff94488eb420cab7013d4d8
david <4504352+northernocean@users.noreply.github.com>
2020-05-12 23:28:41 +0000

    Add moment.js for time parsing view data

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -10,7 +10,8 @@ namespace GigHub
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/umd/popper.js",
-                        "~/Scripts/underscore.js"));
+                        "~/Scripts/underscore.js",
+                        "~/Scripts/moment.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
