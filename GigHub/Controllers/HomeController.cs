f485342e689865ead08a89fcd90d89a66f4c2965
david <4504352+northernocean@users.noreply.github.com>
2020-05-16 18:13:47 +0000

    Update gigs calendar to correctly show if user is attending.

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
@@ -32,12 +34,22 @@ namespace GigHub.Controllers
                                 g.Venue.Contains(query));
             }
 
+            IEnumerable<int> userAttendances = new List<int>();
+            if (User.Identity.IsAuthenticated)
+            {
+                var userId = User.Identity.GetUserId();
+                userAttendances = _context.Attendances
+                    .Where(a => a.AttendeeId == userId)
+                    .Select(g => g.GigId).ToList();
+            }
+
             var viewModel = new GigsViewModel
             {
                 upcomingGigs = upcomingGigs,
                 ShowActions = User.Identity.IsAuthenticated,
                 Heading = "Upcoming Gigs",
-                SearchTerm = query
+                SearchTerm = query,
+                Attendances = userAttendances
             };
 
             return View("Gigs", viewModel);
--- GigHub/ViewModels/GigsViewModel.cs
+++ GigHub/ViewModels/GigsViewModel.cs
@@ -7,7 +7,9 @@ namespace GigHub.ViewModels
     {
         public IEnumerable<Gig> upcomingGigs { get; set; }
         public bool ShowActions { get; set; }
-        public string Heading { get; set; }
-        public string SearchTerm { get; set; }
+        public string Heading { get; set; } = "";
+        public string SearchTerm { get; set; } = "";
+        public IEnumerable<int> Attendances { get; set; }
+        public string AuthenticatedUserId { get; set; } = "";
     }
 }
